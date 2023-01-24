d232152bcd2b9dad62951ce82d7beb0ae5c42064
OmerKilicc <omeerfkiliic@gmail.com>
2023-01-24 13:26:50 +0300

    Buttons DONE

---
+++ Assets/AnimationForButtons.cs
@@ -0,0 +1,28 @@
+using Euphrates;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimationForButtons : MonoBehaviour
+{
+    [Header("Events")]
+    [SerializeField] TriggerChannelSO _lastLayerTrigger;
+    [Space]
+    [SerializeField] Button _nextButton;
+    [SerializeField] Button _finishButton;
+
+    private void OnEnable()
+    {
+        _lastLayerTrigger.AddListener(OnLastLayer);
+    }
+    private void OnDisable()
+    {
+        _lastLayerTrigger.RemoveListener(OnLastLayer);
+    }
+    private void OnLastLayer()
+    {
+        _nextButton.interactable = false;
+        _finishButton.interactable = true;
+    }
+}
